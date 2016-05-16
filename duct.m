683e39c64df9cd21ff8f62915c434c0170fb94ce
unknown <mima7556@INTERNAL.USYD.EDU.AU>
2016-05-16 12:43:03 +1000

    Comments and modifications to get working code.

--- duct.m
+++ duct.m
@@ -1,7 +1,65 @@
+% First order Euler scheme time discretisation
+% Approximate pressure at the new time step and use Poisson equation
+
+%%
+% code timing -----------------------------------------------------------------
+tic;
+%
+% v1, no optimisations, 12.5635 s
+
+%%
+% given parameters ------------------------------------------------------------
+nu     = 0.001;
+rho    = 1;
+Uin    = 1.0;
+length = 4;
+height = 0.1;
+
+
+%%
+% solver settings -------------------------------------------------------------
+
+% discretisation controls
+dt = 1e-3;
+hx = 1e-3;
+hy = 1e-3;
+
+% set number of mesh nodes in x and y directions
+% ghost cells at edges to maintain boundary conditions
+nhx = length/hx + 2;
+nhy = height/hy + 2;
+
+% stopping criteria
+U_change_max = 1e-3;
+
+
+%%
+% initialize variables --------------------------------------------------------
+
+% number of time step iterations needed to find solution
+n_count = 0;
+
+% set initial change in velocity to get in loop
+U_change = 10;
+
+% mesh matrixes to compute U velocity, V velocity, and pressure
+U    = zeros(nhx, nhy);
+Unew = zeros(nhx, nhy);
+V    = zeros(nhx, nhy);
+Vnew = zeros(nhy, nhx);
+P    = zeros(nhx, nhy);
+
+[nhx, nhy] = size(U); % matrix is reversed with rows as x, columns as y
+
+U(1, :) = Uin; % inlet on left
+
+%%
+% calculate the solution ------------------------------------------------------
 
 while  U_change > U_change_max
   n_count=n_count+1;
 
+  % calculate intermediate U velocity
   for i=2:nhx-1
     for j=2:nhy-1
       Unew(i,j)= U(i,j)...
@@ -16,6 +74,7 @@ while  U_change > U_change_max
   Unew(2:nhx,1)=-Unew(2:nhx,2);
   Unew(2:nhx,nhy)=-Unew(2:nhx,nhy-1);
 
+  % calculate intermediate V velocity
   for i=2:nhx-1
     for j=2:nhy-1
       Vnew(i,j)= V(i,j)...
@@ -30,6 +89,7 @@ while  U_change > U_change_max
   Vnew(:,1)=-Vnew(:,2);
   Vnew(:,nhy)=-Vnew(:,nhy-1);
 
+  % compute divergence at each node
   for i=2:nhx-1
     for j=2:nhy-1
       div(i,j)=(Unew(i+1,j)-Unew(i-1,j))/(2*hx)...
@@ -37,18 +97,28 @@ while  U_change > U_change_max
     end
   end
 
+  % calculate average divergence over all nodes
   div_sum=sum(sum(abs(div)))/((nhx-2)*(nhy-2));
 
+  % solve Poisson equation for pressure as heat equation
   resid_pc=1;
+  resid_pc_max = 1e-3;
   p_count=0;
+
+  % relaxation factor
+  relx_pc = 0.1;
 
   while resid_pc > resid_pc_max & p_count < 100;
     p_count=p_count+1;
     for i=2:nhx-1
       for j=2:nhy-1
+
+        % error
         residual(i,j)= -1/(hx*hx)*(P(i+1,j)-2.*P(i,j)+P(i-1,j))...
                        -1/(hy*hy)*(P(i,j+1)-2.*P(i,j)+P(i,j-1))...
                        +(rho/dt)*div(i,j);
+
+
         P(i,j)= (1/(-2/(hx*hx)-2/(hy*hy))*residual(i,j))*relx_pc+P(i,j);
       end
     end
@@ -62,6 +132,7 @@ while  U_change > U_change_max
 
   end
 
+  % correct velocity based on pressure results
   for i=2:nhx-1
     for j=2:nhy-1
       Unew(i,j)= Unew(i,j)-(dt/rho)*(P(i+1,j)-P(i-1,j))/(2*hx);
@@ -69,6 +140,7 @@ while  U_change > U_change_max
     end
   end
 
+  % enforce boundary conditions
   Unew(nhx,:)=Unew(nhx-1,:);
   Unew(1,2:nhy-1)=-Unew(2,2:nhy-1)+2;
   Unew(2:nhx,1)=-Unew(2:nhx,2);
@@ -91,6 +163,7 @@ while  U_change > U_change_max
   V = Vnew;
   Vnew = V;
   
+  % display results every 10 time steps
   if round(n_count/10)*10 == n_count 
     n_count
     div_sum
@@ -101,3 +174,5 @@ while  U_change > U_change_max
 end
 
 U_change
+
+toc
